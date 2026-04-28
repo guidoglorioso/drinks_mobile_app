fbcb47c57297cd478e5e3b28835222da3843a826
Guido Glorioso <guido.glorioso@gmail.com>
2026-04-28 16:59:41 -0300

    Agrego opcion de buscar bebidas con buscador flotante

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,8 +1,11 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:login_app/domain/drink.dart';
 import 'package:login_app/domain/repository_drinks.dart';
 
+// widget principal
 class HomeScreen extends StatefulWidget {
   final String userEmail;
 
@@ -23,23 +26,115 @@ class _HomeScreenState extends State<HomeScreen>{
     drinks = RepositoryDrinks().drinks; // valor inicial
   }
 
-  void actualizarLista( List<Drink> newDrinks) {
+  void searchDrinks( String busqueda) {
     setState(() {
-      drinks = newDrinks;
+      String busquedaLowerCase = busqueda.toLowerCase();
+      List<Drink> drinksBuff = RepositoryDrinks().drinks;
+      drinks = drinksBuff.where((ret) => ret.name.toLowerCase().contains(busquedaLowerCase)).toList();
     }
   );
   }
 
+
   @override
   Widget build(BuildContext context) {
     //final textStyle = Theme.of(context).textTheme;
+
     return Scaffold(
       appBar: AppBar(title: const Text('FuckinDrinks!!')),
       body: _DrinkList(drinks: drinks),
-      
+      floatingActionButton: SearchBarManager(onSearch: (busqueda) => searchDrinks(busqueda) ,),
       );
   }
 }
+
+class SearchBarManager extends StatefulWidget {
+    
+  final Function(String) onSearch;
+
+  const SearchBarManager({super.key,required this.onSearch});
+
+  @override
+  State<SearchBarManager> createState() => _SearchBarManager();
+
+}
+
+class _SearchBarManager extends State<SearchBarManager>{
+  OverlayEntry? _overlayEntry;
+  final FocusNode _searchFocusNode = FocusNode();
+
+  void showSearchBar(){
+    
+    if(_overlayEntry == null){
+      _overlayEntry = createSearchBar(context);
+      Overlay.of(context).insert(_overlayEntry!);
+
+      // foco en el cuadro de busqueda
+      _searchFocusNode.requestFocus();
+    }
+
+  }
+
+  void hideSearchBar(){
+
+    if(_overlayEntry != null){
+      _overlayEntry?.remove();
+      _overlayEntry = null;
+    }
+    
+  }
+
+  void submitedSearchBar(String busqueda){
+    hideSearchBar();
+    widget.onSearch(busqueda);
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+      return FloatingActionButton(
+        onPressed: showSearchBar,
+        child: Icon(Icons.search),
+        );
+  }
+
+  OverlayEntry createSearchBar(BuildContext context){
+    return OverlayEntry(
+      builder: (context) => Stack(
+
+        // Primer widget
+        children: [
+          GestureDetector(
+            onTap:  () => hideSearchBar(), 
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 5,sigmaY: 5),
+              child: Container(
+                color: Colors.transparent.withOpacity(0.1),
+              ),
+            ),
+          ),
+        
+        // Segundo widget
+        Positioned(
+          bottom: MediaQuery.of(context).size.height / 2,
+          left: MediaQuery.of(context).size.width * 0.1,
+          width: MediaQuery.of(context).size.width * 0.8,
+          child: Material(
+            color: Colors.transparent,
+            child: SearchBar(
+              focusNode: _searchFocusNode,
+              hintText: "Busqueda", 
+              onSubmitted: (value) => submitedSearchBar(value),
+            ),
+          ),
+        ),
+        ], 
+      ),
+    );
+  }
+
+}
+
 
 
 class _DrinkList extends StatelessWidget {
@@ -87,3 +182,4 @@ class _ItemDrink extends StatelessWidget {
     );
   }
 }
+
